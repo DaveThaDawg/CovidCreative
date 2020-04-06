946c89a4b897d27b365517c0117b54f764c18c75
Dave <davethadawg@gmail.com>
2020-04-06 14:50:36 +0100

    Add project files.

---
+++ CovidCreative/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CovidCreative.Startup))]
+namespace CovidCreative
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
